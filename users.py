1b57cee5066e2c1b3eddfea1dbd234301a277470
Artyom Lipovoy <oskarualds@mail.ru>
2021-01-09 22:43:47 +0300

    Erick Mattis 'study python'

---
+++ users.py
@@ -0,0 +1,34 @@
+class User():
+    "registered users info"
+    def __init__(self, first_name, last_name, age, login_attempts=1):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.login_attempts = login_attempts
+
+    def describe_user(self):
+        print (f'user info: {self.first_name} {self.last_name}, age: {self.age}')
+        print (f'number of logins: {self.login_attempts}')
+
+    def greeting_user(self):
+        print (f'Hello, mister or missis {self.first_name} {self.last_name}')
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+id1 = User('Stan', 'Smith', 45)
+
+id1.describe_user()
+id1.greeting_user()
+
+id1.increment_login_attempts()
+id1.describe_user()
+id1.increment_login_attempts()
+id1.describe_user()
+id1.reset_login_attempts()
+id1.describe_user()    
+
+    

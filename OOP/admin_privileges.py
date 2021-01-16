1cf35685f16b150cdd517cac7c1bc85bea538c8b
Artyom Lipovoy <oskarualds@mail.ru>
2021-01-16 18:40:42 +0300

    add comments

--- OOP/admin_privileges.py
+++ OOP/admin_privileges.py
@@ -1,6 +1,7 @@
 from users import User
 
 class Privileges():
+    "describe  privileges for user with admin rights"
     def __init__(self):
         self.privileges = ['adding messages granted', 'erasing message granted', 'bannig users granted']
 
@@ -8,6 +9,7 @@ class Privileges():
         print (f'You privileges are: {self.privileges}')
 
 class Admin (User):
+    "describe user admin"
     def __init__(self, first_name, last_name, age, login_attempts=0):
         super().__init__(first_name, last_name, age, login_attempts=0)
         self.privileges = Privileges()
